162b8eb0cd0665310422a063bce62cb5cd43808d
oscarvera <oscarveratomas@gmail.com>
2014-05-07 18:37:38 +0200

    Añadido keylistener a los campos de Ingreso Eliminado la decoracion del JFrame de Ingreso

--- AdRestaurant/src/Ingreso.java
+++ AdRestaurant/src/Ingreso.java
@@ -7,6 +7,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,8 +21,10 @@ import javax.swing.JTextField;
 public class Ingreso extends JPanel{
 
 	private JFrame frame;
-	private JTextField txtN;
-	private JPasswordField pwdKp;
+	private JTextField textLabelUser;
+	private JPasswordField textLabelPass;
+	private JButton btnIngresar;
+	private JButton btnRegistrar;
 
 	/**
 	 * Launch the application.
@@ -55,7 +59,10 @@ public class Ingreso extends JPanel{
 		frame.setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
 		frame.setBounds(100, 100, 895, 646);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultLookAndFeelDecorated(true);
+		frame.setUndecorated(true);
 		frame.getContentPane().setLayout(null);
+		
 		
 		JLabel lblBienvenid = new JLabel("BIENVENID@");
 		lblBienvenid.setBounds(377, 163, 135, 64);
@@ -77,17 +84,39 @@ public class Ingreso extends JPanel{
 		lblUsuario.setBounds(237, 281, 100, 22);
 		frame.getContentPane().add(lblUsuario);
 		
-		txtN = new JTextField();
-		txtN.setToolTipText("Introduzca su Nombre de Usuario");
-		txtN.setColumns(10);
-		txtN.setBounds(339, 270, 227, 37);
-		frame.getContentPane().add(txtN);
+		textLabelUser = new JTextField();
+		textLabelUser.setToolTipText("Introduzca su Nombre de Usuario");
+		textLabelUser.setColumns(10);
+		textLabelUser.setBounds(339, 270, 227, 37);
+		frame.getContentPane().add(textLabelUser);
+		KeyListener kl = new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {	
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if(!textLabelUser.getText().isEmpty()&&!textLabelPass.getText().isEmpty()){
+				btnIngresar.setEnabled(true);
+			}else{
+				btnIngresar.setEnabled(false);
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				
+			}
+		};
+		textLabelUser.addKeyListener(kl);
 		
-		JLabel lblContrasea = new JLabel("CONTRASE\u00D1A:");
-		lblContrasea.setForeground(Color.WHITE);
-		lblContrasea.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
-		lblContrasea.setBounds(219, 327, 114, 22);
-		frame.getContentPane().add(lblContrasea);
+		JLabel lblPass = new JLabel("CONTRASE\u00D1A:");
+		lblPass.setForeground(Color.WHITE);
+		lblPass.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
+		lblPass.setBounds(219, 327, 114, 22);
+		frame.getContentPane().add(lblPass);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 255, 255));
@@ -111,41 +140,45 @@ public class Ingreso extends JPanel{
 		gbc_lblnoEstasRegistrado.gridy = 1;
 		panel.add(lblnoEstasRegistrado, gbc_lblnoEstasRegistrado);
 		
-		JButton btnNewButton_1 = new JButton("REGISTRARSE");
-		btnNewButton_1.setToolTipText("Simple y cómodo, REGISTRATE AHORA!");
-		btnNewButton_1.setBackground(new Color(255, 153, 51));
-		btnNewButton_1.setForeground(new Color(255, 255, 255));
-		btnNewButton_1.addActionListener(new ActionListener() {
+		btnRegistrar = new JButton("REGISTRARSE");
+		btnRegistrar.setToolTipText("Simple y cómodo, REGISTRATE AHORA!");
+		btnRegistrar.setBackground(new Color(255, 153, 51));
+		btnRegistrar.setForeground(new Color(255, 255, 255));
+		btnRegistrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				RegistroCliente regcli=new RegistroCliente();
 				frame.dispose();
 			
 			}
 		});
-		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
-		gbc_btnNewButton_1.fill = GridBagConstraints.BOTH;
-		gbc_btnNewButton_1.gridx = 4;
-		gbc_btnNewButton_1.gridy = 1;
-		panel.add(btnNewButton_1, gbc_btnNewButton_1);
+		GridBagConstraints gbc_btnRegistrar = new GridBagConstraints();
+		gbc_btnRegistrar.fill = GridBagConstraints.BOTH;
+		gbc_btnRegistrar.gridx = 4;
+		gbc_btnRegistrar.gridy = 1;
+		panel.add(btnRegistrar, gbc_btnRegistrar);
 		
-		JButton btnNewButton = new JButton("INGRESAR");
+		btnIngresar = new JButton("INGRESAR");
 		
-		btnNewButton.setBackground(new Color(255, 255, 255));
-		btnNewButton.setForeground(new Color(255, 153, 51));
-		btnNewButton.setFont(new Font("Fira Sans OT", Font.PLAIN, 11));
-		btnNewButton.addActionListener(new ActionListener() {
+		btnIngresar.setBackground(new Color(255, 255, 255));
+		btnIngresar.setForeground(new Color(255, 153, 51));
+		btnIngresar.setFont(new Font("Fira Sans OT", Font.PLAIN, 11));
+		btnIngresar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				MenuCliente menuclie=new MenuCliente();
 				frame.dispose();
 			}
 		});
-		btnNewButton.setBounds(339, 374, 227, 37);
-		frame.getContentPane().add(btnNewButton);
+		btnIngresar.setBounds(339, 374, 227, 37);
+		btnIngresar.setEnabled(false);
+		frame.getContentPane().add(btnIngresar);
 		
-		pwdKp = new JPasswordField();
-		pwdKp.setToolTipText("Introduzca su contraseña");
-		pwdKp.setBounds(339, 321, 227, 37);
-		frame.getContentPane().add(pwdKp);
+		
+		textLabelPass = new JPasswordField();
+		textLabelPass.setToolTipText("Introduzca su contraseña");
+		textLabelPass.setBounds(339, 321, 227, 37);
+		frame.getContentPane().add(textLabelPass);
 		frame.setVisible(true);
+		textLabelPass.addKeyListener(kl);
+		
 	}
 }
