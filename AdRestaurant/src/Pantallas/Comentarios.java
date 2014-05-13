98cc7f4b05f97bb0239bf221cd9587f4d2d7dd5e
Mario <sanogueralorenzo@gmail.com>
2014-05-13 13:36:24 +0200

    Mario Corrected Clase Comentarios que por error tenia la clase Comentario

--- AdRestaurant/src/Pantallas/Comentarios.java
+++ AdRestaurant/src/Pantallas/Comentarios.java
@@ -1,70 +1,168 @@
-package Clases;
+package Pantallas;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Comentario {
-	
-	String nombreRestaurante;
-	String usuarioComentario;
-	String hora;
-	String fechaCreacion;
-	
-	//Comentario ya creado, recepción de datos
-	
-	public Comentario(String nomRest, String user,String fechaCrea){
-		nomRest=this.nombreRestaurante;
-		user=this.usuarioComentario;
-		fechaCrea=this.fechaCreacion;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+
+import Clases.Cliente;
+
+public class Comentarios extends JFrame {
+
+	private JPanel contentPane;
+	private JFrame frame;
+	Cliente clie;
+
+	/**
+	 * Launch the application.
+	 */
+	/*public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Comentarios frame = new Comentarios();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}*/
+
+	/**
+	 * Create the frame.
+	 */
+	public Comentarios( final Cliente clie) {
+		this.clie=clie;
+		frame = new JFrame();		
+		frame.setResizable(false);
+		frame.getContentPane().setBackground(new Color(255, 153, 0));
+		frame.getContentPane().setForeground(Color.LIGHT_GRAY);
+		frame.setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
+		frame.setBounds(100, 100, 895, 646);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultLookAndFeelDecorated(true);
+		frame.setUndecorated(true);
+		frame.getContentPane().setLayout(null);
 		
-		//consultar hora fechaReserva verificación realizacion
-	}
-	
-	//Reserva no creada. Para creacion.
-	
-	public Comentario(String nomRest, String user, String fechaReserva, String hora, String fechaCrea, int personas){
-		nomRest=this.nombreRestaurante;
-		user=this.usuarioComentario;
-		hora=this.hora;
-		fechaCrea=this.fechaCreacion;
 		
-		//Creacion de la consulta 
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 86, 889, 481);
+		frame.getContentPane().add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 153, 0));
+		
+		JList list = new JList();
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Aqui", "los ", "restaurantes", "listados", "todos", "y", "cada", "uno", "de", "ellos"};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
+					.addGap(18)
+					.addComponent(list, GroupLayout.PREFERRED_SIZE, 711, GroupLayout.PREFERRED_SIZE)
+					.addGap(26))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 481, Short.MAX_VALUE)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(21)
+					.addComponent(list, GroupLayout.PREFERRED_SIZE, 439, GroupLayout.PREFERRED_SIZE)
+					.addGap(21))
+		);
+		panel_1.setLayout(null);
+		panel.setLayout(gl_panel);
+		
+		JLabel label = new JLabel("AdRestaurant");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Francois One", Font.PLAIN, 60));
+		label.setBackground(Color.WHITE);
+		label.setBounds(22, 1, 371, 74);
+		frame.getContentPane().add(label);
+		
+		JLabel lblnomUser = new JLabel(clie.getNombre());
+		lblnomUser.setForeground(Color.WHITE);
+		lblnomUser.setFont(new Font("Fira Sans OT", Font.PLAIN, 20));
+		lblnomUser.setBackground(Color.YELLOW);
+		lblnomUser.setBounds(541, 11, 128, 64);
+		frame.getContentPane().add(lblnomUser);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Salir salir= new Salir(clie);
+				frame.dispose();
+			}
+		});
+		btnSalir.setForeground(new Color(255, 255, 255));
+		btnSalir.setBackground(new Color(255, 153, 51));
+		btnSalir.setBounds(823, 34, 66, 23);
+		frame.getContentPane().add(btnSalir);
+		
+		JLabel lblBuscarRestaurante = new JLabel("Tus Comentarios");
+		lblBuscarRestaurante.setForeground(Color.WHITE);
+		lblBuscarRestaurante.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 16));
+		lblBuscarRestaurante.setBackground(Color.YELLOW);
+		lblBuscarRestaurante.setBounds(391, 11, 140, 64);
+		frame.getContentPane().add(lblBuscarRestaurante);
+		
+		JButton btnMenuPrincipal = new JButton("Volver al men\u00FA principal");
+		btnMenuPrincipal.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
+		btnMenuPrincipal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MenuCliente mencli=new MenuCliente(clie);
+				frame.dispose();
+			}
+		});
+		btnMenuPrincipal.setBackground(new Color(255, 153, 0));
+		btnMenuPrincipal.setForeground(new Color(255, 255, 255));
+		btnMenuPrincipal.setBounds(22, 578, 171, 23);
+		frame.getContentPane().add(btnMenuPrincipal);
+		
+		JButton btnCerrarSesion = new JButton("Cerrar Sesi\u00F3n");
+		btnCerrarSesion.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
+		btnCerrarSesion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Ingreso ingreso= new Ingreso();
+				frame.dispose();
+			}
+		});
+		btnCerrarSesion.setForeground(Color.WHITE);
+		btnCerrarSesion.setBackground(new Color(255, 153, 51));
+		btnCerrarSesion.setBounds(685, 34, 128, 23);
+		frame.getContentPane().add(btnCerrarSesion);
+		frame.setBounds(100, 100, 895, 646);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
-	
-	//GETTERS
-	
-	public String getNomRest(){
-		return this.nombreRestaurante;
-	}
-	public String getUsuario(){
-		return this.usuarioComentario;
-	}
-	public String getHora(){
-		return this.hora;
-	}
-	public String getFechaCrea(){
-		return this.fechaCreacion;
-	}
-	//SETTERS CON ACTUALIZACION
-	
-	public void setNomRest(String nomRest){
-		this.nombreRestaurante=nomRest;
-		this.actualizacionComentario();
-	}
-	public void setUsuario(String user){
-		this.usuarioComentario=user;
-		this.actualizacionComentario();
-	}
-	public void setHora( String horaReser){
-		this.hora=horaReser;
-		this.actualizacionComentario();
-	}
-	public void setFechaCrea( String fechaCrea){
-		this.fechaCreacion=fechaCrea;
-		this.actualizacionComentario();
-	}
-	
-	public void actualizacionComentario(){
-		//Actualizacion de los datos de la consulta
-	
-	}
-	
-	
+
 }
