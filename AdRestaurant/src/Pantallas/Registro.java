482023414e23b5de340e50e8c4ec1de22409ea27
Mario <Mario@PC>
2014-05-18 23:38:16 +0200

    Mario Traducido Registro menos variable comboTipoRest

--- AdRestaurant/src/Pantallas/Registro.java
+++ AdRestaurant/src/Pantallas/Registro.java
@@ -36,6 +36,7 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.SwingConstants;
 
 public class Registro extends JPanel{
 
@@ -65,12 +66,13 @@ public class Registro extends JPanel{
 	private JComboBox comboTipoRest;
 	
 	private JLabel Flecha1;
-	private JLabel LabelNuevoClie;
 	private JLabel Flecha2;
-	private JLabel LabelNuevoRest;
 	
 	static Locale currentLocale;
     static ResourceBundle messages;
+    
+    private JLabel lblNC;
+    private JLabel lblNR;
 
 	/**
 	 * Create the application.
@@ -102,16 +104,16 @@ public class Registro extends JPanel{
 		lblAdrestaurant.setFont(new Font("Francois One", Font.PLAIN, 90));
 		frame.getContentPane().add(lblAdrestaurant);
 		
-		btnCliente = new JButton("CLIENTE");
+		btnCliente = new JButton(messages.getString("CLIENTE"));
 		btnCliente.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
 		btnCliente.setForeground(new Color(255, 153, 0));
 		btnCliente.setBackground(Color.WHITE);
 		btnCliente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				LabelNuevoClie.setVisible(false);
+				lblNC.setVisible(false);
 				Flecha1.setVisible(false);
 				Flecha2.setVisible(false);
-				LabelNuevoRest.setVisible(false);
+				lblNR.setVisible(false);
 				
 				panel.removeAll();
 				panel.setBackground(new Color(255, 255, 255));
@@ -141,7 +143,7 @@ public class Registro extends JPanel{
 					}
 				};
 				
-				JLabel lblNombreUsuario = new JLabel("Nombre usuario:");
+				JLabel lblNombreUsuario = new JLabel(messages.getString("NU"));
 				lblNombreUsuario.setForeground(new Color(255, 153, 51));
 				lblNombreUsuario.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblNombreUsuario.setBounds(258, 145, 155, 22);
@@ -153,13 +155,13 @@ public class Registro extends JPanel{
 				panel.add(textNomUser);
 				textNomUser.addKeyListener(keyLis);
 				
-				JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+				JLabel lblContrasea = new JLabel(messages.getString("Pass"));
 				lblContrasea.setForeground(new Color(255, 153, 51));
 				lblContrasea.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblContrasea.setBounds(258, 178, 155, 22);
 				panel.add(lblContrasea);
 				
-				JLabel lblEmail = new JLabel("Email:");
+				JLabel lblEmail = new JLabel(messages.getString("Email"));
 				lblEmail.setForeground(new Color(255, 153, 0));
 				lblEmail.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblEmail.setBounds(258, 211, 155, 22);
@@ -177,7 +179,7 @@ public class Registro extends JPanel{
 				panel.add(textNombre);
 				textNombre.addKeyListener(keyLis);
 				
-				JLabel lblNombre = new JLabel("Nombre:");
+				JLabel lblNombre = new JLabel(messages.getString("Nombre"));
 				lblNombre.setForeground(new Color(255, 153, 0));
 				lblNombre.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblNombre.setBounds(258, 46, 155, 22);
@@ -189,7 +191,7 @@ public class Registro extends JPanel{
 				panel.add(textApellido1);
 				textApellido1.addKeyListener(keyLis);
 				
-				JLabel lblPrimerApellido = new JLabel("Primer apellido:");
+				JLabel lblPrimerApellido = new JLabel(messages.getString("PA"));
 				lblPrimerApellido.setForeground(new Color(255, 153, 51));
 				lblPrimerApellido.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblPrimerApellido.setBounds(258, 79, 155, 22);
@@ -201,7 +203,7 @@ public class Registro extends JPanel{
 				panel.add(textApellido2);
 				textApellido2.addKeyListener(keyLis);
 				
-				JLabel lblSegundoApellido = new JLabel("Segundo apellido:");
+				JLabel lblSegundoApellido = new JLabel(messages.getString("SA"));
 				lblSegundoApellido.setForeground(new Color(255, 153, 0));
 				lblSegundoApellido.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblSegundoApellido.setBounds(258, 112, 169, 22);
@@ -211,7 +213,7 @@ public class Registro extends JPanel{
 				 * Botón Registrarse del cliente. Al accionarse llama a comprobarDatos().
 				 */
 				
-				btnRegistrarse = new JButton("REGISTRARSE");
+				btnRegistrarse = new JButton(messages.getString("REGISTRARSE"));
 				btnRegistrarse.setBackground(new Color(255, 153, 0));
 				btnRegistrarse.setForeground(new Color(255, 255, 255));
 				btnRegistrarse.addActionListener(new ActionListener() {
@@ -228,7 +230,7 @@ public class Registro extends JPanel{
 				passUser.setBounds(423, 178, 197, 22);
 				panel.add(passUser);
 				
-				JLabel lblTelfono = new JLabel("Tel\u00E9fono:");
+				JLabel lblTelfono = new JLabel(messages.getString("Telefono"));
 				lblTelfono.setForeground(new Color(255, 153, 0));
 				lblTelfono.setFont(new Font("Dialog", Font.BOLD, 18));
 				lblTelfono.setBounds(258, 244, 104, 22);
@@ -249,15 +251,15 @@ public class Registro extends JPanel{
 		btnCliente.setBounds(165, 137, 263, 36);
 		frame.getContentPane().add(btnCliente);
 		
-		btnRestaurante = new JButton("RESTAURANTE");
+		btnRestaurante = new JButton(messages.getString("RESTAURANTE"));
 		btnRestaurante.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
 		btnRestaurante.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				LabelNuevoClie.setVisible(false);
+				lblNC.setVisible(false);
 				Flecha1.setVisible(false);
 				Flecha2.setVisible(false);
-				LabelNuevoRest.setVisible(false);
+				lblNR.setVisible(false);
 							
 				panel.removeAll();
 				panel.setBackground(new Color(255, 255, 255));
@@ -293,7 +295,7 @@ public class Registro extends JPanel{
 				panel.add(comboTipoRest);
 				comboTipoRest.addKeyListener(keyLis);
 						
-				JLabel lblNombreUsuario = new JLabel("Nombre usuario:");
+				JLabel lblNombreUsuario = new JLabel(messages.getString("NU"));
 				lblNombreUsuario.setForeground(new Color(255, 153, 51));
 				lblNombreUsuario.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblNombreUsuario.setBounds(54, 213, 155, 22);
@@ -305,13 +307,13 @@ public class Registro extends JPanel{
 				panel.add(textNomUserRest);
 				textNomUserRest.addKeyListener(keyLis);
 				
-				JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+				JLabel lblContrasea = new JLabel(messages.getString("Pass"));
 				lblContrasea.setForeground(new Color(255, 153, 51));
 				lblContrasea.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblContrasea.setBounds(434, 213, 155, 22);
 				panel.add(lblContrasea);
 				
-				JLabel lblEmail = new JLabel("Email:");
+				JLabel lblEmail = new JLabel(messages.getString("Email"));
 				lblEmail.setForeground(new Color(255, 153, 0));
 				lblEmail.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblEmail.setBounds(54, 246, 155, 22);
@@ -329,13 +331,13 @@ public class Registro extends JPanel{
 				panel.add(textNombreRest);
 				textNombreRest.addKeyListener(keyLis);
 				
-				JLabel lblNombre_1 = new JLabel("Nombre:");
+				JLabel lblNombre_1 = new JLabel(messages.getString("Nombre"));
 				lblNombre_1.setForeground(new Color(255, 153, 0));
 				lblNombre_1.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblNombre_1.setBounds(54, 27, 155, 22);
 				panel.add(lblNombre_1);
 				
-				btnRegRest = new JButton("REGISTRARSE");
+				btnRegRest = new JButton(messages.getString("REGISTRARSE"));
 				btnRegRest.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
 				btnRegRest.setBackground(new Color(255, 153, 0));
 				btnRegRest.setForeground(new Color(255, 255, 255));
@@ -348,7 +350,7 @@ public class Registro extends JPanel{
 				btnRegRest.setEnabled(false);
 				panel.add(btnRegRest);
 				
-				JLabel lblProvincia = new JLabel("Provincia:");
+				JLabel lblProvincia = new JLabel(messages.getString("Provincia"));
 				lblProvincia.setForeground(new Color(255, 153, 51));
 				lblProvincia.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblProvincia.setBounds(476, 60, 155, 22);
@@ -360,7 +362,7 @@ public class Registro extends JPanel{
 				panel.add(textProvinciaRest);
 				textProvinciaRest.addKeyListener(keyLis);
 				
-				JLabel lblPais = new JLabel("C\u00F3digo Postal:");
+				JLabel lblPais = new JLabel(messages.getString("CP"));
 				lblPais.setForeground(new Color(255, 153, 51));
 				lblPais.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblPais.setBounds(476, 93, 155, 22);
@@ -372,7 +374,7 @@ public class Registro extends JPanel{
 				panel.add(textCodPostRest);
 				textCodPostRest.addKeyListener(keyLis);
 				
-				JLabel lblPoblacion = new JLabel("Poblaci\u00F3n:");
+				JLabel lblPoblacion = new JLabel(messages.getString("Poblacion"));
 				lblPoblacion.setForeground(new Color(255, 153, 0));
 				lblPoblacion.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblPoblacion.setBounds(476, 27, 155, 22);
@@ -390,13 +392,13 @@ public class Registro extends JPanel{
 				panel.add(textDireccionRest);
 				textDireccionRest.addKeyListener(keyLis);
 				
-				JLabel lblDireccin = new JLabel("Direcci\u00F3n:");
+				JLabel lblDireccin = new JLabel(messages.getString("Direccion"));
 				lblDireccin.setForeground(new Color(255, 153, 51));
 				lblDireccin.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblDireccin.setBounds(54, 60, 155, 22);
 				panel.add(lblDireccin);
 				
-				JLabel lblTipo = new JLabel("Tipo:");
+				JLabel lblTipo = new JLabel (messages.getString("Tipo"));
 				lblTipo.setForeground(new Color(255, 153, 0));
 				lblTipo.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblTipo.setBounds(54, 94, 169, 22);
@@ -409,7 +411,7 @@ public class Registro extends JPanel{
 				
 				
 				
-				JLabel lblAptoParaliticos = new JLabel("Apto paraliticos:");
+				JLabel lblAptoParaliticos = new JLabel(messages.getString("AP"));
 				lblAptoParaliticos.setForeground(new Color(255, 153, 0));
 				lblAptoParaliticos.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblAptoParaliticos.setBounds(54, 127, 155, 22);
@@ -420,13 +422,13 @@ public class Registro extends JPanel{
 				chckbxNewCheckBox.setBounds(219, 126, 27, 23);
 				panel.add(chckbxNewCheckBox);
 				
-				JLabel lblImagen1 = new JLabel("Im\u00E1gen 1:");
+				JLabel lblImagen1 = new JLabel(messages.getString("IMG1"));
 				lblImagen1.setForeground(new Color(255, 153, 51));
 				lblImagen1.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblImagen1.setBounds(318, 127, 92, 22);
 				panel.add(lblImagen1);
 				
-				JLabel lblImagen2 = new JLabel("Im\u00E1gen 2:");
+				JLabel lblImagen2 = new JLabel(messages.getString("IMG2"));
 				lblImagen2.setForeground(new Color(255, 153, 51));
 				lblImagen2.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblImagen2.setBounds(577, 127, 92, 22);
@@ -437,7 +439,7 @@ public class Registro extends JPanel{
 				panel_11.setBounds(34, 176, 821, 5);
 				panel.add(panel_11);
 				
-				JLabel lblTelefono = new JLabel("Tel\u00E9fono:");
+				JLabel lblTelefono = new JLabel(messages.getString("Telefono"));
 				lblTelefono.setForeground(new Color(255, 153, 51));
 				lblTelefono.setFont(new Font("Fira Sans OT Light", Font.BOLD, 18));
 				lblTelefono.setBounds(434, 246, 155, 22);
@@ -460,14 +462,14 @@ public class Registro extends JPanel{
 		
 		
 		
-		JLabel lblyaEstaRegistrado = new JLabel("\u00BFYa est\u00E1s registrado?");
+		JLabel lblyaEstaRegistrado = new JLabel(messages.getString("YAREG"));
 		lblyaEstaRegistrado.setForeground(new Color(255, 255, 255));
 		lblyaEstaRegistrado.setFont(new Font("Fira Sans OT", Font.PLAIN, 16));
 		lblyaEstaRegistrado.setBackground(new Color(255, 153, 51));
 		lblyaEstaRegistrado.setBounds(182, 564, 216, 34);
 		frame.getContentPane().add(lblyaEstaRegistrado);
 		
-		JButton btnIniciarSesin = new JButton("INICIAR SESI\u00D3N");
+		JButton btnIniciarSesin = new JButton (messages.getString("IS"));
 		btnIniciarSesin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Ingreso ingreso=new Ingreso();
@@ -480,7 +482,7 @@ public class Registro extends JPanel{
 		btnIniciarSesin.setBounds(24, 573, 148, 19);
 		frame.getContentPane().add(btnIniciarSesin);
 		
-		JLabel lblInformacionSobreUsuarios = new JLabel("Informaci\u00F3n sobre usuarios");
+		JLabel lblInformacionSobreUsuarios = new JLabel (messages.getString("INFOUSER"));
 		lblInformacionSobreUsuarios.setForeground(Color.WHITE);
 		lblInformacionSobreUsuarios.setFont(new Font("Fira Sans OT", Font.PLAIN, 15));
 		lblInformacionSobreUsuarios.setBackground(new Color(255, 153, 51));
@@ -492,20 +494,24 @@ public class Registro extends JPanel{
 		Flecha1.setBounds(175, 184, 185, 151);
 		frame.getContentPane().add(Flecha1);
 		
-		LabelNuevoClie = new JLabel("New label");
-		LabelNuevoClie.setIcon(new ImageIcon(Registro.class.getResource("/Imagen/eresUnNuevoCliente.png")));
-		LabelNuevoClie.setBounds(10, 298, 287, 93);
-		frame.getContentPane().add(LabelNuevoClie);
-		
 		Flecha2 = new JLabel("New label");
 		Flecha2.setIcon(new ImageIcon(Registro.class.getResource("/Imagen/flecha2.png")));
 		Flecha2.setBounds(467, 195, 250, 209);
 		frame.getContentPane().add(Flecha2);
 		
-		LabelNuevoRest = new JLabel("New label");
-		LabelNuevoRest.setIcon(new ImageIcon(Registro.class.getResource("/Imagen/eresUnNuevoRestaurante.png")));
-		LabelNuevoRest.setBounds(488, 402, 346, 36);
-		frame.getContentPane().add(LabelNuevoRest);
+		lblNC = new JLabel(messages.getString("NC"));
+		lblNC.setForeground(Color.WHITE);
+		lblNC.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNC.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 20));
+		lblNC.setBounds(24, 327, 404, 36);
+		frame.getContentPane().add(lblNC);
+		
+		lblNR = new JLabel(messages.getString("NR"));
+		lblNR.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNR.setForeground(Color.WHITE);
+		lblNR.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 20));
+		lblNR.setBounds(465, 389, 404, 36);
+		frame.getContentPane().add(lblNR);
 		
 		frame.setVisible(true);
 	}
@@ -540,7 +546,7 @@ public class Registro extends JPanel{
 		Pattern pat = Pattern.compile("[0-9]{9}");
 	    Matcher mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Teléfono. Introduce 9 números entre 0 y 9.");
+	    	mensajesError.add(messages.getString("ErrorTELF"));
 	    	componentesError.add("textTelefono");
 	    } 
 	    
@@ -549,7 +555,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Nombre. Introduce sólo letras."); 
+	    	mensajesError.add(messages.getString("ErrorNOMBRE"));
 	    	componentesError.add("textNombre");
 	    } 
 	    
@@ -558,7 +564,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Primer Apellido. Introduce sólo letras.");
+	    	mensajesError.add(messages.getString("Error1APELLIDO"));
 	    	componentesError.add("textApellido1");
 	    } 
 	    
@@ -567,7 +573,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Segundo Apellido. Introduce sólo letras."); 
+	    	mensajesError.add(messages.getString("Error2APELLIDO"));
 	    	componentesError.add("textApellido2");
 	    } 
 	    
@@ -576,7 +582,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z0-9]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Nombre de Usuario. Introduce sólo letras o números"); 
+	    	mensajesError.add(messages.getString("ErrorNOMBREUSUARIO")); 
 	    	componentesError.add("textNomUser");
 	    } 
 	    
@@ -585,7 +591,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Email. Siga la estructura, ejemplo: X____@''dominio''.__");
+	    	mensajesError.add(messages.getString("ErrorEMAIL")); 
 	    	componentesError.add("textEmail");
 	    } 
 	    
@@ -613,7 +619,7 @@ public class Registro extends JPanel{
 		Pattern pat = Pattern.compile("[0-9]{9}");
 	    Matcher mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Teléfono. Introduce 9 números entre 0 y 9."); 
+	    	mensajesError.add(messages.getString("ErrorTELF")); 
 	    	componentesError.add("textTelefonoRest");
 	    } 
 	    
@@ -622,7 +628,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z0-9]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Nombre. Introduce sólo letras."); 
+	    	mensajesError.add(messages.getString("ErrorNOMBRE")); 
 	    	componentesError.add("textNombreRest");
 	    } 
 	    
@@ -631,7 +637,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z0-9]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Nombre de Usuario.");
+	    	mensajesError.add(messages.getString("ErrorNOMBREUSUARIO")); 
 	    	componentesError.add("textNomUserRest");
 	    } 
 	    
@@ -640,7 +646,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Email. Siga la estructura, ejemplo: X____@''dominio''.__");
+	    	mensajesError.add(messages.getString("ErrorEMAIL")); 
 	    	componentesError.add("textEmailRest");
 	    } 
 	    
@@ -649,7 +655,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z0-9]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Direccion. Introduce sólo letras y el numero de la puerta.");
+	    	mensajesError.add(messages.getString("ErrorDIRECCION")); 
 	    	componentesError.add("textDireccionRest");
 	    } 
 	    
@@ -658,7 +664,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Poblacion. Introduce una poblacion.");
+	    	mensajesError.add(messages.getString("ErrorPOBLACION")); 
 	    	componentesError.add("textPoblacionRest");
 	    } 
 	    
@@ -667,7 +673,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[a-zA-Z]+");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Provincia. Introduce una provincia."); 
+	    	mensajesError.add(messages.getString("ErrorPROVINCIA")); 
 	    	componentesError.add("textProvinciaRest");
 	    } 
 	    
@@ -676,7 +682,7 @@ public class Registro extends JPanel{
 		pat = Pattern.compile("[0-9]{5}");
 	    mat = pat.matcher(textoIntroducido);
 	    if (!mat.matches()){
-	    	mensajesError.add("Error en el campo Codigo Postal. Introduce solo cinco numeros."); 
+	    	mensajesError.add(messages.getString("ErrorCP")); 
 	    	componentesError.add("textCodPostRest");
 	    } 
 	    
