c607a9ea265afb0e1e0835aad0cc1ed55f09680c
Mario <Mario@PC>
2014-05-25 16:17:24 +0200

    Mario Redistribuido JList, incrementado el tamaño del Nombre y JList cambiada fuente y corregido al color naranja correcto (new Color(255,153,0));

--- AdRestaurant/src/Pantallas/InfoRestaurante.java
+++ AdRestaurant/src/Pantallas/InfoRestaurante.java
@@ -18,6 +18,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
 import javax.swing.border.SoftBevelBorder;
+import java.awt.Font;
 
 public class InfoRestaurante extends JPanel  {
 	private JLabel etiqueta_nombre;
@@ -36,43 +37,47 @@ public class InfoRestaurante extends JPanel  {
 		this.codigo=codigo;
 		
 		this.etiqueta_nombre = new JLabel(nombre);
-		etiqueta_nombre.setForeground(Color.ORANGE);
+		etiqueta_nombre.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 16));
+		etiqueta_nombre.setForeground(new Color(255,153,0));
 		
 		this.etiqueta_tipo = new JLabel(tipo);
-		etiqueta_tipo.setForeground(Color.ORANGE);
+		etiqueta_tipo.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 16));
+		etiqueta_tipo.setForeground(new Color(255,153,0));
 		
 		this.etiqueta_poblacion= new JLabel(poblacion);
-		etiqueta_poblacion.setForeground(Color.ORANGE);
+		etiqueta_poblacion.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
+		etiqueta_poblacion.setForeground(new Color(255,153,0));
 		
 		this.etiqueta_direccion = new JLabel(direccion);
-		etiqueta_direccion.setForeground(Color.ORANGE);
+		etiqueta_direccion.setFont(new Font("Fira Sans OT Light", Font.PLAIN, 12));
+		etiqueta_direccion.setForeground(new Color(255,153,0));
 		GroupLayout groupLayout = new GroupLayout(this);
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(etiqueta_tipo)
-						.addComponent(etiqueta_nombre))
-					.addGap(35)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(etiqueta_poblacion)
-						.addComponent(etiqueta_direccion))
-					.addGap(60))
+					.addComponent(etiqueta_nombre, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(etiqueta_tipo, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(etiqueta_direccion)
+						.addComponent(etiqueta_poblacion))
+					.addGap(95))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(etiqueta_nombre)
-						.addComponent(etiqueta_direccion))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(etiqueta_tipo)
-						.addComponent(etiqueta_poblacion))
-					.addContainerGap())
-					//.addContainerGap(194, Short.MAX_VALUE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(etiqueta_direccion)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(etiqueta_poblacion))
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(etiqueta_nombre, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+							.addComponent(etiqueta_tipo, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(253, Short.MAX_VALUE))
 		);
 		setLayout(groupLayout);
 		
